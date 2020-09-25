068f31f3892e70597dc985351e130cd13163d6d1
Jon Wear <jon@jonwear.comm>
2020-09-25 15:30:47 -0400

    let's take this back

--- main.go
+++ main.go
@@ -1,71 +1,67 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/x509"
-	"encoding/asn1"
-	"encoding/gob"
-	"encoding/pem"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 func main() {
-	name := flag.String("name", "temp", "The name of the key pair to be created.  They will be created as name_key.rsa and name_key.rsa.pub")
-	size := flag.Int("size", 4096, "Size of the RSA Key.  Default is 4096")
+	name := flag.String("name", "temp", "The of the private key file to be used to sign the JWT")
+	aud := flag.String("aud", "none", "Audeince(aud) for the JWT.  If left blank no JWT will be created.  This is typcally the service that will be verifying and extracting data from the JWT to do something.")
+	sub := flag.String("sub", "none", "Subject(sub) for the JWT.  If left blank no JWT will be created.  This is typcally the calling service that is supplying the JWT to the specified AUD service.")
+	exp := flag.Int("exp", 0, "Expiration(exp) hours from current unix time for the JWT expiration. If left blank no JWT will be created.")
 	flag.Parse()
-	reader := rand.Reader
 
-	key, err := rsa.GenerateKey(reader, *size)
-	checkError(err)
+	privName := *name + "rsa"
 
-	publicKey := key.PublicKey
-	savePEMKey(*name+"_key.rsa", key)
-	savePublicPEMKey(*name+"_key.rsa.pub", publicKey)
-
-}
-
-func saveGobKey(fileName string, key interface{}) {
-	outFile, err := os.Create(fileName)
-	checkError(err)
-	defer outFile.Close()
-
-	encoder := gob.NewEncoder(outFile)
-	err = encoder.Encode(key)
-	checkError(err)
-}
-
-func savePEMKey(fileName string, key *rsa.PrivateKey) {
-	outFile, err := os.Create(fileName)
-	checkError(err)
-	defer outFile.Close()
-
-	var privateKey = &pem.Block{
-		Type:  "PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	if len(*aud) > 0 && len(*sub) > 0 && *exp > 0 {
+		jwt, jErr := makeJWT(privName, *aud, *sub, *exp)
+		if jErr != nil {
+			log.Println(jErr)
+		}
+		fmt.Println(jwt)
 	}
 
-	err = pem.Encode(outFile, privateKey)
-	checkError(err)
 }
 
-func savePublicPEMKey(fileName string, pubkey rsa.PublicKey) {
-	asn1Bytes, err := asn1.Marshal(pubkey)
-	checkError(err)
-
-	var pemkey = &pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: asn1Bytes,
+func makeJWT(privepath, aud, sub string, exp int) (string, error) {
+	n := time.Now()
+	signBytes, err := ioutil.ReadFile(privepath)
+	if err != nil {
+		log.Fatal(err)
 	}
+	signKey, keyErr := x509.ParsePKCS1PrivateKey(signBytes)
+	if keyErr != nil {
+		log.Println(keyErr)
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+		"sub": sub,
+		"nbf": n.Unix(),
+		"exp": n.Add(time.Hour * time.Duration(exp)),
+		"aud": aud,
+		"iat": n.Unix(),
+	})
 
-	pemfile, err := os.Create(fileName)
-	checkError(err)
-	defer pemfile.Close()
+	// Sign and get the complete encoded token as a string using the secret
+	return token.SignedString(signKey)
 
-	err = pem.Encode(pemfile, pemkey)
+}
+
+func saveJWT(filename, token string) {
+	outFile, err := os.Create(filename)
 	checkError(err)
+	defer outFile.Close()
+	_, err2 := io.WriteString(outFile, token)
+	checkError(err2)
+	outFile.Sync()
 }
 
 func checkError(err error) {
