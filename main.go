f11a7d5612bdbf4ca5148a90f09f5fda75ef538a
Jon Wear <jon@jonwear.comm>
2020-11-11 12:14:36 -0500

    this is a revert back, something went wrong

--- main.go
+++ main.go
@@ -23,44 +23,41 @@ import (
 // openssl rsa -in app.rsa -pubout > app.rsa.pub
 
 func main() {
-	name := flag.String("name", "temp", "The is the base name of the private key file to be used to sign the JWT. If the file is called private.rsa you would just enter 'private'.")
-	file := flag.String("file", "tempfile", "This is the name of the pre-existing rsa file to use to make a new JWT.  You cannot specify -name and -file at the same time")
+	name := flag.String("name", "temp", "The of the base name of the private key file to be used to sign the JWT. If the file is called private.rsa you would just enter 'private'.")
 	bsize := flag.Int("size", 4096, "Bitsize of the RSA key.  The default is 4096.")
 	sub := flag.String("sub", "", "Subject(sub) for the JWT.  If left blank no JWT will be created.  The subject is typically the source/signer of the JWT.")
 	aud := flag.String("aud", "", "Audience(aud) for the JWT.  If left blank no JWT will be created.  This is typcally the service that will be verifying and extracting data from the JWT to do something.")
 	exp := flag.Int("exp", 0, "Expiration(exp) hours from current unix time for the JWT expiration. If left blank no JWT will be created.")
 	flag.Parse()
-	privkeyname := ""
-	if *file != "tempfile" {
-		privkeyname = *file
-	} else {
-		if err := makeRSAKeys(*name, *bsize); err != nil {
-			log.Println(err)
-			return
-		}
-		privkeyname = *name + ".rsa"
-		savePubKeyToBase64(*name)
+
+	if err := makeRSAKeys(*name, *bsize); err != nil {
+		log.Println(err)
+		return
 	}
+
 	if len(*aud) > 0 && *exp > 0 && len(*sub) > 0 {
-		log.Println("PrivKey:", privkeyname)
-		jwt, jErr := makeJWT(privkeyname, *aud, *name, *exp)
+		jwt, jErr := makeJWT(*name+".rsa", *aud, *name, *exp)
 		if jErr != nil {
-			log.Println("makeJWT:", jErr)
+			log.Println(jErr)
 		}
-		saveJWT(privkeyname, jwt)
+		saveJWT(*name+".rsa", jwt)
+	} else {
+		log.Println("No JWT created.")
 	}
+	savePubKeyToBase64(*name)
+
 }
 
 func makeJWT(privepath, aud, sub string, exp int) (string, error) {
 	n := time.Now()
-	log.Print("PrivPath:", privepath)
 	signBytes, err := ioutil.ReadFile(privepath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	signKey, keyErr := ssh.ParsePrivateKey(signBytes)
+	signKey, keyErr := ssh.ParseRawPrivateKey(signBytes)
+
 	if keyErr != nil {
-		log.Println("Parse Error:", keyErr)
+		log.Println(keyErr)
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
 		"sub": sub,
@@ -82,6 +79,7 @@ func saveJWT(filename, token string) {
 	_, err2 := io.WriteString(outFile, token)
 	checkError(err2)
 	outFile.Sync()
+	log.Println(filename + ".jwt")
 }
 
 func savePubKeyToBase64(name string) {
@@ -91,6 +89,7 @@ func savePubKeyToBase64(name string) {
 	sEnc := b64.StdEncoding.EncodeToString(data)
 	err2 := ioutil.WriteFile(name+".pub.base64", []byte(sEnc), 0644)
 	checkError(err2)
+	log.Println(name + ".pub.base64")
 }
 
 func checkError(err error) {
@@ -109,14 +108,13 @@ func makeRSAKeys(filename string, size int) error {
 	// Generate RSA key.
 	key, err := rsa.GenerateKey(rand.Reader, size)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	// Extract public component.
 	pub := key.Public()
 
 	// Encode private key to PKCS#1 ASN.1 PEM.
-
 	keyPEM := pem.EncodeToMemory(
 		&pem.Block{
 			Type:  "RSA PRIVATE KEY",
@@ -124,18 +122,14 @@ func makeRSAKeys(filename string, size int) error {
 		},
 	)
 
-	// Encode public key to PKIXP PEM.
-
-	pubBytes, pbErr := x509.MarshalPKIXPublicKey(pub.(*rsa.PublicKey))
-	if pbErr != nil {
-		log.Fatal(pbErr)
-	}
+	// Encode public key to PKCS#1 ASN.1 PEM.
 	pubPEM := pem.EncodeToMemory(
 		&pem.Block{
-			Type:  "PUBLIC KEY",
-			Bytes: pubBytes,
+			Type:  "RSA PUBLIC KEY",
+			Bytes: x509.MarshalPKCS1PublicKey(pub.(*rsa.PublicKey)),
 		},
 	)
+
 	// Write private key to file.
 	if err := ioutil.WriteFile(filename+".rsa", keyPEM, 0700); err != nil {
 		return err
