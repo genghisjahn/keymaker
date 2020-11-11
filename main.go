b9d9844e3838adeb72515f8c3e0e4a0dfd2ed1ba
Jon Wear <jon@jonwear.comm>
2020-11-11 11:53:31 -0500

    tried update code, this doesn't work right

--- main.go
+++ main.go
@@ -24,39 +24,43 @@ import (
 
 func main() {
 	name := flag.String("name", "temp", "The is the base name of the private key file to be used to sign the JWT. If the file is called private.rsa you would just enter 'private'.")
+	file := flag.String("file", "tempfile", "This is the name of the pre-existing rsa file to use to make a new JWT.  You cannot specify -name and -file at the same time")
 	bsize := flag.Int("size", 4096, "Bitsize of the RSA key.  The default is 4096.")
 	sub := flag.String("sub", "", "Subject(sub) for the JWT.  If left blank no JWT will be created.  The subject is typically the source/signer of the JWT.")
 	aud := flag.String("aud", "", "Audience(aud) for the JWT.  If left blank no JWT will be created.  This is typcally the service that will be verifying and extracting data from the JWT to do something.")
 	exp := flag.Int("exp", 0, "Expiration(exp) hours from current unix time for the JWT expiration. If left blank no JWT will be created.")
 	flag.Parse()
-
-	if err := makeRSAKeys(*name, *bsize); err != nil {
-		log.Println(err)
-		return
-	}
-
-	if len(*aud) > 0 && *exp > 0 && len(*sub) > 0 {
-		jwt, jErr := makeJWT(*name+".rsa", *aud, *name, *exp)
-		if jErr != nil {
-			log.Println(jErr)
-		}
-		saveJWT(*name+".rsa", jwt)
+	privkeyname := ""
+	if *file != "tempfile" {
+		privkeyname = *file
 	} else {
-		log.Println("No JWT created.")
+		if err := makeRSAKeys(*name, *bsize); err != nil {
+			log.Println(err)
+			return
+		}
+		privkeyname = *name + ".rsa"
+		savePubKeyToBase64(*name)
 	}
-	savePubKeyToBase64(*name)
-
+	if len(*aud) > 0 && *exp > 0 && len(*sub) > 0 {
+		log.Println("PrivKey:", privkeyname)
+		jwt, jErr := makeJWT(privkeyname, *aud, *name, *exp)
+		if jErr != nil {
+			log.Println("makeJWT:", jErr)
+		}
+		saveJWT(privkeyname, jwt)
+	}
 }
 
 func makeJWT(privepath, aud, sub string, exp int) (string, error) {
 	n := time.Now()
+	log.Print("PrivPath:", privepath)
 	signBytes, err := ioutil.ReadFile(privepath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	signKey, keyErr := ssh.ParsePrivateKey(signBytes)
 	if keyErr != nil {
-		log.Println(keyErr)
+		log.Println("Parse Error:", keyErr)
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
 		"sub": sub,
@@ -78,7 +82,6 @@ func saveJWT(filename, token string) {
 	_, err2 := io.WriteString(outFile, token)
 	checkError(err2)
 	outFile.Sync()
-	log.Println(filename + ".jwt")
 }
 
 func savePubKeyToBase64(name string) {
@@ -88,7 +91,6 @@ func savePubKeyToBase64(name string) {
 	sEnc := b64.StdEncoding.EncodeToString(data)
 	err2 := ioutil.WriteFile(name+".pub.base64", []byte(sEnc), 0644)
 	checkError(err2)
-	log.Println(name + ".pub.base64")
 }
 
 func checkError(err error) {
