fadc657c65798cd7978fc85033c9608077b5bd2a
Jon Wear <jon.wear@warnerbros.com>
2021-04-22 12:36:59 -0400

    update scripts

--- makejwt.sh
+++ makejwt.sh
@@ -3,32 +3,33 @@
 #   How to use this shell script....
 #
 #
-#   makejwt.sh uses 4 parameters
+#   makejwt.sh uses 5 parameters
+#   #1 is the kid or Key ID that identifies the key pair used to sign/identify the token.
 #
-#   $1 is the path to the file that contains the proto-jwt claims that will be put into the final, signed JWT
+#   $2 is the path to the file that contains the proto-jwt claims that will be put into the final, signed JWT
 #      There are place holder values in this file for iat, nbf and exp
 #   
-#   $2 is the number of hours that will be added to the current time for the JWT's expiration
+#   $3 is the number of hours that will be added to the current time for the JWT's expiration
 #
-#   $3 is the path to the private rsa key that will be used to sign the JWT
+#   $4 is the path to the private rsa key that will be used to sign the JWT
 # 
-#   $4 is name file that the signed JWT will be output to
+#   $5 is name file that the signed JWT will be output to
 #
 #   Example:
 
-#   ./climakejwt.sh proto.json 10 example.rsa example.jwt
+#   ./climakejwt.sh 12345678 proto.json 10 example.rsa example.jwt
 #   
-#   The above command will open the proto.json ($1) file, 
-#   replace place holder values for iat, nbf with the current unix/epoch time
-#   replace place holder value exp  with current time + 10 ($2) hours as unix/epoch time
+#   The above command will set the "kid" value in the header to 12345678,
+#   pen the proto.json ($2) file, 
+#   replace place holder values for iat, nbf with the current unix/epoch time,
+#   replace place holder value exp  with current time + 10 ($3) hours as unix/epoch time,
 #   put the header, payload and signtaure together into one string, 
-#   create the signature based on that string with the private key in the file example.rsa ($3)
-#   output the result to the file example.jwt ($4)
-
-jwt_header=$(echo -n '{"alg":"RS256","typ":"JWT"}' | base64 | sed s/\+/-/ | sed -E s/=+$//)
+#   create the signature based on that string with the private key in the file example.rsa ($4),
+#   output the result to the file example.jwt ($5).
+jwt_header=$(echo -n '{"alg":"RS256","typ":"JWT","kid":"'$1'"}' | base64 | sed s/\+/-/ | sed -E s/=+$//)
 t=$(date +%s)
-exptime=$(date -j -v +$2H +%s)
-filepayload=`cat $1`
+exptime=$(date -j -v +$3H +%s)
+filepayload=`cat $2`
 filepayload=${filepayload/\"iat_value\"/$t}
 filepayload=${filepayload/\"nbf_value\"/$t}
 filepayload=${filepayload/\"exp_value\"/ $exptime}
@@ -36,7 +37,7 @@ payload=$(echo -n $filepayload | base64 | sed s/\+/-/ | sed -E s/=+$//)
 
 body=${jwt_header}.${payload}
 
-signature=$(echo -n $body | openssl dgst -sha256 -binary -sign $3  | openssl enc -base64 | tr -d '\n=' | tr -- '+/' '-_')
+signature=$(echo -n $body | openssl dgst -sha256 -binary -sign $4  | openssl enc -base64 | tr -d '\n=' | tr -- '+/' '-_')
   
 jwt=${jwt_header}.${payload}.${signature}
-echo $jwt > $4
+echo $jwt > $5
--- makekeys.sh
+++ makekeys.sh
@@ -17,13 +17,17 @@
 
 # Example:
 #
-#   ./makekeys.sh keyfiles/test1.rsa 4096
+#   ./makekeys.sh keyfiles/test1.rsa 2048
 #
 #   The makekeys.sh shell script will create a private key at the location keyfiles/test1.rsa
-#   It will have a keysize of 4096 bits
+#   It will have a keysize of 2048 bits
 #   The public key will be created at the location keyfiles/test1.rsa.pub
 #   The base64 encoded public key will be created at keyfiles/test1.rsa.pub.base64
+#   A kid or Key ID
 
 openssl genrsa -out $1 $2
 openssl rsa -in $1 -pubout > $1.pub
 base64 $1.pub > $1.pub.base64
+kid=$(xxd -l 4 -c 4 -p < /dev/random)
+echo "You must put this value in the kid (key id) of the JWT: " $kid
+
